9b29a97e19cc9702c7830cf15fae7568e333ad8f
Ivan Giuliani <giuliani.v@gmail.com>
2016-10-09 21:13:59 +0100

    Correctly load the configuration objects

--- app/bot/init.rb
+++ app/bot/init.rb
@@ -4,13 +4,12 @@ require_relative "config"
 
 module Bot
   module Startup
-    extend Bot::Config
     extend Discordrb::EventContainer
     extend Loggy
 
     ready do |event|
       bot = event.bot
-      bot.profile.username = cfg_username
+      bot.profile.username = CONFIG.username
 
       log "Bot connected and ready, say hello to #{bot.profile.username}."
       log "Invite via #{bot.invite_url}"
