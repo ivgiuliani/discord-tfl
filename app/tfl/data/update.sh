973791cbc1f8b22f39b2acfa1c2ca61fbd5ae2c4
Ivan Giuliani <giuliani.v@gmail.com>
2016-10-14 22:02:55 +0100

    Use ruby to download json files so that we can get pretty printing

--- app/tfl/data/update.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Updates data for common modes (see `tfl/const.rb`).
-
-wget "https://api.tfl.gov.uk/Line/Mode/bus" -O "bus_lines.json"
-wget "https://api.tfl.gov.uk/Line/Mode/national-rail" -O "national_rail_lines.json"
-wget "https://api.tfl.gov.uk/Line/Mode/river-bus" -O "river_bus_lines.json"
-wget "https://api.tfl.gov.uk/Line/Mode/river-tour" -O "river_tour_lines.json"
