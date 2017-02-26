38b05cf670b7964cac802712d8b23c0de45248e2
Ivan Giuliani <giuliani.v@gmail.com>
2017-02-26 23:14:27 +0000

    Improve launcher
    Also add a better support for exceptions in the rufus scheduler.

--- app.rb
+++ app.rb
@@ -2,31 +2,40 @@
 require "sentry-raven-without-integrations"
 require "./app/tflbot"
 
+module RufusRavenSupport
+  def on_error(job, err)
+    Raven.capture_exception(err, extra: { "job": job })
+    super
+  end
+end
+
 # We have to monkey patch the rufus scheduler to support sentry because there's
 # no direct way to override the default exception handler.
 module Rufus
   class Scheduler
-    def on_error(job, err)
-      Raven.capture_exception(err, extra: { "job": job })
-    end
+    prepend RufusRavenSupport
   end
 end
 
-bot = Bot::LondonBot.new
+def run
+  bot = Bot::LondonBot.new
 
-begin
-  bot.run!
-rescue SignalException
-  # Best effort attempt at sending a close-frame to discord. API calls might
-  # not be confirmed without a close frame, resulting in unexpected behaviour.
-  # See discussion on https://github.com/meew0/discordrb/pull/251#issuecomment-254053322
-  # for more details.
-  bot.stop!
+  begin
+    bot.run!
+  rescue SignalException
+    # Best effort attempt at sending a close-frame to discord. API calls might
+    # not be confirmed without a close frame, resulting in unexpected behaviour.
+    # See discussion on https://github.com/meew0/discordrb/pull/251#issuecomment-254053322
+    # for more details.
+    bot.stop!
 
-  $stdout.puts "So long and thanks for all the fish."
-  exit! true
-# rubocop:disable Lint/RescueException
-rescue Exception => exception
-  Raven.capture_exception(exception)
+    $stdout.puts "So long and thanks for all the fish."
+    exit! true
+  # rubocop:disable Lint/RescueException
+  rescue Exception => exception
+    Raven.capture_exception(exception)
+  end
+  # rubocop:enable Lint/RescueException
 end
-# rubocop:enable Lint/RescueException
+
+run
