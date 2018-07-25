5ba2115884f8d35eb6c7ae79f8dfd7bb726713a6
Ivan Giuliani <giuliani.v@gmail.com>
2018-07-25 22:50:53 +0100

    Add !bus command
    Returns first and last stop's name, plus a count of all stops in
    between.

---
+++ app/bot/commands/bus.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+require_relative "../discord_utils"
+
+module Bot
+  module Commands
+    class BusCommand
+      include Loggy
+
+      COMMAND = :bus
+
+      def self.execute(event)
+        BusCommand.new.execute(event)
+      end
+
+      def execute(event)
+        return if event.from_bot?
+
+        args = Commands.split_args(COMMAND, event)
+        args = DiscordUtils.filter_mentions(*args, mentions: event.message.mentions)
+        return unless valid_query?(args)
+
+        bus_line = args.first
+
+        log "[command/bus(from:#{event.user.name})] #{bus_line}"
+
+        begin
+          bus = TFL.bus_route(bus_line.strip)
+
+          # \u2192 is a unicode right arrow. The double-ended arrow which would arguably
+          # be more correct in this case is displayed as an emoji in Discord for reasons
+          # that are beyond me.
+          "**#{bus.name}**: " \
+            "#{bus.stop_points.first.name} \u2192 #{bus.stop_points.last.name} " \
+            "(#{bus.stop_points.count} stops)"
+        rescue Tfl::InvalidRouteException, Tfl::InvalidStopPointException
+          event << "#{bus_line}: invalid bus line"
+          return
+        rescue Songkick::Transport::TimeoutError
+          event << "#{DiscordUtils::Emoji::SCREAM} Request timed out (blame TfL)"
+          return
+        end
+      end
+
+      def valid_query?(args)
+        args.count == 1 && Tfl::Const::Bus::ALL.include?(args.first.strip.downcase)
+      end
+    end
+  end
+end
